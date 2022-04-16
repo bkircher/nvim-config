6971fe7006ca3850d9d8e338b56e2912683f5635
Ben Kircher <bkircher@0xadd.de>
2022-04-16 16:16:15 +0200

    options: Keep some space when scrolling off

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,6 +6,8 @@ local options = {
   wrap = true, -- Do not wrap lines in the middle of a word
   linebreak = true,
   cursorline = true, -- Highlight the current line
+  scrolloff = 4, -- Keep some screen lines below, above, or right to the cursor
+  sidescrolloff = 8,
 }
 
 for k, v in pairs(options) do
