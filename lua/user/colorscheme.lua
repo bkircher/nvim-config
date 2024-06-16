9c065472fdfc14c9fc158fbffc25a97e48b86db1
Ben Kircher <bkircher@0xadd.de>
2024-06-16 10:30:27 +0200

    Use new dark default colorscheme everywhere

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,13 +1,11 @@
 -- Since 0.10, Neovim comes with it's own new default color scheme
 -- https://gpanders.com/blog/whats-new-in-neovim-0.10/#colorscheme
--- Set here the classic Vim color scheme because it works better with
--- Solarized light.
 vim.cmd [[
   try
-    colorscheme vim
-    set background=light
-  catch /^Vim\%((\a\+)\)\=:E185/
     colorscheme default
+    set background=dark
+  catch /^Vim\%((\a\+)\)\=:E185/
+    colorscheme vim
     set background=light
   endtry
 ]]
