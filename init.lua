c4ffc524505f43a368b10c29d7feed4f339037a4
Ben Kircher <bkircher@0xadd.de>
2025-09-14 23:23:51 +0200

    Disable language providers

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ end
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = ","
 
+-- Disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 require "user.options"
 require "user.colorscheme"
 require "user.rpmspec"
