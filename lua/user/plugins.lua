85c527dac48624755297a4b5aab873f82be1f0e1
Ben Kircher <bkircher@0xadd.de>
2026-04-08 10:11:06 +0200

    plugins: enable :Undotree and :DiffTool

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,3 +2,7 @@
 -- Highlighting is provided by core via vim.treesitter.start() (see autocmds.lua).
 -- Keep the plugin for parser install/update and indentexpr support.
 pcall(require, "nvim-treesitter")
+
+-- Builtin opt plugins (Neovim 0.12+)
+vim.cmd.packadd('nvim.undotree')
+vim.cmd.packadd('nvim.difftool')
