3fd04fd4f4ca1a0e6d6d2486eb62427eaf9bb107
Ben Kircher <bkircher@0xadd.de>
2026-05-08 13:57:57 +0200

    Guard Neovim 0.12-only features

--- lua/user/newui.lua
+++ lua/user/newui.lua
@@ -5,4 +5,6 @@
 --
 -- Type `g<` to move into the output.
 
-require("vim._core.ui2").enable({})
+if vim.fn.has("nvim-0.12") == 1 then
+	require("vim._core.ui2").enable({})
+end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,5 +4,8 @@
 pcall(require, "nvim-treesitter")
 
 -- Builtin opt plugins (Neovim 0.12+)
-vim.cmd.packadd("nvim.undotree")
-vim.cmd.packadd("nvim.difftool")
+-- Guard these so older Neovim versions can still start cleanly.
+if vim.fn.has("nvim-0.12") == 1 then
+	vim.cmd.packadd("nvim.undotree")
+	vim.cmd.packadd("nvim.difftool")
+end
