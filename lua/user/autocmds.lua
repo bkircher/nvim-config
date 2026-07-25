ab5b6baba6094546f9c4feb5374653d697e85b70
Ben Kircher <bkircher@0xadd.de>
2026-07-25 11:54:20 +0200

    Fix zsh indentation

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -32,6 +32,19 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Use four-space indentation for shell scripts
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  desc = "Configure shell script indentation",
+  pattern = { "bash", "sh", "zsh" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
+
 -- Enable spellchecking for text files
 vim.api.nvim_create_autocmd("FileType", {
   group = group,
