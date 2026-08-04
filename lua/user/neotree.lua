e81e345b06cdfb03eef16b42a234dda72d6a2c73
Ben Kircher <bkircher@0xadd.de>
2026-08-04 08:55:22 +0200

    Disable Neo-tree icons

--- lua/user/neotree.lua
+++ lua/user/neotree.lua
@@ -1,3 +1,11 @@
+require("neo-tree").setup({
+  default_component_configs = {
+    icon = {
+      enabled = false,
+    },
+  },
+})
+
 vim.keymap.set("n", "<leader>b", "<Cmd>Neotree toggle<CR>", {
   desc = "Toggle file tree",
   silent = true,
