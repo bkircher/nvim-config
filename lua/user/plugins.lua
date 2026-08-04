56b089d6aab099ca05aad4ea37e551949cde5ed1
Ben Kircher <bkircher@0xadd.de>
2026-08-04 08:05:44 +0200

    Add Neo-tree

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,6 +2,12 @@ vim.pack.add({
   "https://github.com/sainnhe/everforest.git",
   "https://github.com/nvim-treesitter/nvim-treesitter.git",
   "https://github.com/nvim-lua/plenary.nvim.git",
+  {
+    src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+    version = vim.version.range("3"),
+  },
+  "https://github.com/MunifTanjim/nui.nvim",
+  "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/nvim-telescope/telescope.nvim.git",
 })
 
